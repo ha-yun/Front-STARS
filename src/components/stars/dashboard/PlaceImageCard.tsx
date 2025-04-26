590d8d5a6d8d43d24612aae5515e77a9b1af82f0
Jungu Lee <prnsslcn@gmail.com>
2025-04-26 14:06:20 +0900

    Refactor: 반응형 대시보드 적용
    - 각 카드별 col-span 반응형 적용

--- src/components/stars/dashboard/PlaceImageCard.tsx
+++ src/components/stars/dashboard/PlaceImageCard.tsx
@@ -13,7 +13,7 @@ export default function PlaceImageCard({
 }: PlaceImageCardProps) {
     return (
         <motion.div
-            className="col-span-12 bg-white rounded-xl shadow-md p-4 my-2"
+            className="col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-12 bg-white rounded-xl shadow-lg p-4 my-2"
             whileHover={{ y: -6 }}
             animate={style}
             style={style}
