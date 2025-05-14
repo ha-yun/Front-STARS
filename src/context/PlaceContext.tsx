94c40327fee654ff9fbb84d333576ac75977df4d
Jungu Lee <prnsslcn@gmail.com>
2025-05-14 13:59:44 +0900

    refactor: 지역(관광특구) 기반 마커 호출
    관광지 -> 지역 기반 변경

--- src/context/PlaceContext.tsx
+++ src/context/PlaceContext.tsx
@@ -1,21 +1,35 @@
 // src/context/PlaceContext.tsx
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState } from "react";
 
 interface PlaceContextType {
     selectedPlace: string;
     setSelectedPlace: (placeId: string) => void;
     triggerCountUp: boolean;
     setTriggerCountUp: (value: boolean) => void;
+
+    selectedAreaId: string | null;
+    setSelectedAreaId: (areaId: string) => void;
 }
 
 const PlaceContext = createContext<PlaceContextType | undefined>(undefined);
 
 export function PlaceProvider({ children }: { children: React.ReactNode }) {
-    const [selectedPlace, setSelectedPlace] = useState<string>('');
+    const [selectedPlace, setSelectedPlace] = useState<string>("");
     const [triggerCountUp, setTriggerCountUp] = useState<boolean>(false);
 
+    const [selectedAreaId, setSelectedAreaId] = useState<string | null>(null);
+
     return (
-        <PlaceContext.Provider value={{ selectedPlace, setSelectedPlace, triggerCountUp, setTriggerCountUp }}>
+        <PlaceContext.Provider
+            value={{
+                selectedPlace,
+                setSelectedPlace,
+                triggerCountUp,
+                setTriggerCountUp,
+                selectedAreaId,
+                setSelectedAreaId,
+            }}
+        >
             {children}
         </PlaceContext.Provider>
     );
@@ -24,7 +38,7 @@ export function PlaceProvider({ children }: { children: React.ReactNode }) {
 export function usePlace() {
     const context = useContext(PlaceContext);
     if (!context) {
-        throw new Error('usePlace must be used within a PlaceProvider');
+        throw new Error("usePlace must be used within a PlaceProvider");
     }
     return context;
 }
