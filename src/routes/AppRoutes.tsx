c7814b8c36e268e515424a27c32674d97575cb12
Danmark-Jajang <lightning0145@gmail.com>
2025-05-14 15:12:42 +0900

    Feat : Admin 관제 페이지 context화
    데이터를 너무 자주 불러와서 SSE의 연결을 유지하기 위해 상위 container 개념 도입
    container에서 SSE 연결 유지하고, 그 정보를 하위 component에 전달

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,6 +1,8 @@
+// src/routes/AppRoutes.tsx
 import { Routes, Route, Navigate } from "react-router-dom";
 import FullPageLayout from "../layouts/FullPageLayout";
-import AdminPage from "../pages/admin/AdminPage";
+import AdminContainer from "../components/admin/AdminContainer";
+import AdminComponent from "../components/admin/AdminComponent";
 import AdminDetail from "../components/admin/AdminDetail";
 import AdminTour from "../components/admin/AdminTour";
 import AdminUserFavorite from "../components/admin/AdminUserFavorite";
@@ -9,14 +11,18 @@ import LoginPage from "../pages/login/LoginPage";
 export default function AppRoutes() {
     return (
         <Routes>
-            // 홈페이지 리다이렉트 to /map
+            {/* 홈페이지 리다이렉트 to /map */}
             <Route path="/" element={<Navigate to="/map" replace />} />
             <Route path="/map" element={<FullPageLayout />} />
             <Route path="/login" element={<LoginPage />} />
-            <Route path="/manage" element={<AdminPage />} />
-            <Route path="/manage/:spotCode" element={<AdminDetail />} />
-            <Route path="/manage/tour" element={<AdminTour />} />
-            <Route path="/manage/user" element={<AdminUserFavorite />} />
+
+            {/* AdminContainer를 부모 라우트로 설정하고, 그 아래에 중첩 라우트 구성 */}
+            <Route path="/manage" element={<AdminContainer />}>
+                <Route index element={<AdminComponent />} />
+                <Route path=":spotCode" element={<AdminDetail />} />
+                <Route path="tour" element={<AdminTour />} />
+                <Route path="user" element={<AdminUserFavorite />} />
+            </Route>
         </Routes>
     );
 }
